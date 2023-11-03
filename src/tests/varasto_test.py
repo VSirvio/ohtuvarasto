b9e705dd0f4d1a6820a8c4da604b9e7bc518fee1
Ville Sirviö <VSirvio@users.noreply.github.com>
2023-11-03 19:19:16 +0200

    Nosta Varasto-luokan yksikkötestien haarautumakattavuus 100 %:iin

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,32 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_virheellinen_tilavuus_olion_luonnissa(self):
+        varasto = Varasto(-3.7)
+        self.assertAlmostEqual(varasto.tilavuus, 0)
+
+    def test_virheellinen_saldo_olion_luonnissa(self):
+        varasto = Varasto(2.03, -501.0)
+        self.assertAlmostEqual(varasto.saldo, 0)
+
+    def test_negatiivisen_maaran_lisays_varastoon(self):
+        saldo_alussa = self.varasto.saldo
+        self.varasto.lisaa_varastoon(-0.7)
+        self.assertAlmostEqual(self.varasto.saldo, saldo_alussa)
+
+    def test_lisaa_varastoon_liikaa(self):
+        self.varasto.lisaa_varastoon(10.01)
+        self.assertAlmostEqual(self.varasto.saldo, self.varasto.tilavuus)
+
+    def test_ota_varastosta_negatiivinen_maara(self):
+        otettu_maara = self.varasto.ota_varastosta(-0.01)
+        self.assertAlmostEqual(otettu_maara, 0)
+
+    def test_ota_varastosta_liikaa(self):
+        otettu_maara = self.varasto.ota_varastosta(113.9)
+        self.assertAlmostEqual(otettu_maara, 0)
+
+    def test_muunnos_merkkijonoksi(self):
+        mjono = self.varasto.__str__()
+        self.assertEqual(mjono, "saldo = 0, vielä tilaa 10")
